cd22385ca29269cb8159daaa0b061e1e44f3e5b3
thebestharrison1221 <100962097+Thebestharrison1221@users.noreply.github.com>
2024-08-24 13:28:29 +0100

    Update missing_create_recipes.js

--- server_scripts/missing_create_recipes.js
+++ server_scripts/missing_create_recipes.js
@@ -67,11 +67,21 @@ main
      A: 'kubejs:unfired_steel_ingot' 
    })
 
-   //9 ingot to 1 block
-   event.shapeless('9x unfired_steel_ingot', [
+    //andesite alloy sheet fix
+   event.shapeless('1x unfired_steel_ingot', [
      'unfired_steel_block',
    ])
-
+    
+   //1 ingot to 1 block
+   event.shapeless('1x create_dd:andesite_sheet', [
+     'createdeco:andesite_sheet',
+   ])
+   
+    //9 ingot to 1 block
+   event.shapeless('1x createdeco:andesite_sheet', [
+     'create_dd:andesite_sheet',
+   ])
+    
    //unfired to unpolished
    ServerEvents.recipes(event => {
     event.recipes.create.filling('kubejs:unpolished_steel_block', [Fluid.lava(), 'kubejs:steel_block'])
